fe4cca8b5df4459647f3d378711d8264df1558f5
Serkan ŞAHİN <serkansahin@yahoo.com>
2017-01-18 07:47:31 +0200

    Add files via upload

---
+++ timeline/content.php
@@ -0,0 +1,42 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title">Günlükler</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <table class="table table-bordered">
+          <tr>
+            <th>#</th>
+            <th>Başlık</th>
+            <th>İçerik</th>
+            <th>Tarih</th>
+            <th>İşlemler</th>
+          </tr>
+          <tr>
+            <td>1.</td>
+            <td>Harika bir gün!!</td>
+            <td>Harun hoca dersi iptal ettiği için çok mutlu oldum.</td>
+            <td>17 Ocak 2017 18:56</td>
+            <td>Düzenle | Sil</td>
+          </tr>
+          
+        </table>
+      </div>
+      <!-- /.box-body -->
+      <div class="box-footer clearfix">
+        <ul class="pagination pagination-sm no-margin pull-right">
+          <li><a href="#">&laquo;</a></li>
+          <li><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">&raquo;</a></li>
+        </ul>
+      </div>
+    </div>   <!-- /.box -->
+    <!-- /.box -->
+  </div>
+  <!-- /.col -->
+  <!-- /.col -->
+</div>
